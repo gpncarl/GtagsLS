89ce9450686903a22210bbfe8814ea7b2ba187f9
guopeining <guopeining@baijia.com>
2022-03-14 02:36:23 +0800

    use root dir to update db

--- src/GtagsLS.hs
+++ src/GtagsLS.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DisambiguateRecordFields #-}
 
 module GtagsLS
     ( handlers
@@ -6,6 +7,7 @@ module GtagsLS
 
 import Language.LSP.Server
 import Language.LSP.Types
+import Language.LSP.Types.Capabilities
 import Control.Monad.IO.Class
 import qualified Data.Text as T
 import Text.Regex.PCRE
@@ -16,8 +18,104 @@ import Control.Exception (evaluate)
 import Control.DeepSeq (force)
 
 handlers :: Handlers (LspM ())
+-- makeExtendingDatatype "InitializeParams" [''WorkDoneProgressParams]
+--   [ ("_processId",             [t| Maybe Int32|])
+--   , ("_clientInfo",            [t| Maybe ClientInfo |])
+--   , ("_rootPath",              [t| Maybe Text |])
+--   , ("_rootUri",               [t| Maybe Uri  |])
+--   , ("_initializationOptions", [t| Maybe Value |])
+--   , ("_capabilities",          [t| ClientCapabilities |])
+--   , ("_trace",                 [t| Maybe Trace |])
+--   , ("_workspaceFolders",      [t| Maybe (List WorkspaceFolder) |])
+--   ]
 handlers = mconcat
-  [ requestHandler STextDocumentDefinition $ \req responder -> do
+  [ requestHandler SInitialize $ \req responder -> do
+      responder $ Right $ InitializeResult
+        { _capabilities = ServerCapabilities
+            { -- | Defines how text documents are synced. Is either a detailed structure
+              -- defining each notification or for backwards compatibility the
+              -- 'TextDocumentSyncKind' number.
+              -- If omitted it defaults to 'TdSyncNone'.
+              _textDocumentSync                 = Nothing
+              -- | The server provides hover support.
+            , _hoverProvider                    = Nothing
+              -- | The server provides completion support.
+            , _completionProvider               = Nothing
+              -- | The server provides signature help support.
+            , _signatureHelpProvider            = Nothing
+              -- | The server provides go to declaration support.
+              --
+              -- Since LSP 3.14.0
+            , _declarationProvider              = Nothing
+              -- | The server provides goto definition support.
+            , _definitionProvider               = Just $ InL True
+              -- | The server provides Goto Type Definition support. Since LSP 3.6
+              --
+              -- @since 0.7.0.0
+            , _typeDefinitionProvider           = Nothing
+              -- | The server provides Goto Implementation support. Since LSP 3.6
+              --
+              -- @since 0.7.0.0
+            , _implementationProvider           = Nothing
+              -- | The server provides find references support.
+            , _referencesProvider               = Just $ InL True
+              -- | The server provides document highlight support.
+            , _documentHighlightProvider        = Nothing
+              -- | The server provides document symbol support.
+            , _documentSymbolProvider           = Just $ InL True
+              -- | The server provides code actions.
+            , _codeActionProvider               = Nothing
+              -- | The server provides code lens.
+            , _codeLensProvider                 = Nothing
+              -- | The server provides document link support.
+            , _documentLinkProvider             = Nothing
+              -- | The server provides color provider support. Since LSP 3.6
+              --
+              -- @since 0.7.0.0
+            , _colorProvider                    = Nothing
+              -- | The server provides document formatting.
+            , _documentFormattingProvider       = Nothing
+              -- | The server provides document range formatting.
+            , _documentRangeFormattingProvider  = Nothing
+              -- | The server provides document formatting on typing.
+            , _documentOnTypeFormattingProvider = Nothing
+              -- | The server provides rename support.
+            , _renameProvider                   = Nothing
+              -- | The server provides folding provider support. Since LSP 3.10
+              --
+              -- @since 0.7.0.0
+            , _foldingRangeProvider             = Nothing
+              -- | The server provides execute command support.
+            , _executeCommandProvider           = Nothing
+              -- | The server provides selection range support. Since LSP 3.15
+            , _selectionRangeProvider           = Nothing
+              -- | The server provides call hierarchy support.
+            -- , _callHierarchyProvider            = Nothing
+            --   -- | The server provides semantic tokens support.
+            --   --
+            --   -- @since 3.16.0
+            -- , _semanticTokensProvider           = Nothing
+              -- | The server provides workspace symbol support.
+            , _workspaceSymbolProvider          = Just True
+              -- | Workspace specific server capabilities
+            , _workspace                        = Nothing
+              -- | Experimental server capabilities.
+            , _experimental                     = Nothing
+            }
+            , _serverInfo = Nothing
+        }
+      env <- getLspEnv 
+      let RequestMessage _ _ _ (InitializeParams _ _ _ rootPath rootUri _ _ _ _) = req
+      liftIO $ runLspT (env { resRootPath = T.unpack <$> rootPath }) (return ())
+  , notificationHandler STextDocumentDidSave $ \notify -> do
+      let NotificationMessage _ _ (DidSaveTextDocumentParams doc _) = notify
+          TextDocumentIdentifier uri = doc
+          Just path = uriToFilePath uri
+      env <- getLspEnv 
+      liftIO $ createProcess (proc "global" ["--verbose", "--single-update", path])
+                { cwd = resRootPath env, std_in = CreatePipe, std_out = CreatePipe, std_err = CreatePipe}
+      return ()
+  , requestHandler STextDocumentDefinition $ \req responder -> do
       let RequestMessage _ _ _ (DefinitionParams doc pos _ _) = req
       word <- liftIO $ wordAtDocPosition doc pos
       res <- liftIO $ globalDR word ""
